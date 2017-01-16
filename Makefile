0f485339b7a19aaa07c484da421a0c463d7dff25
inoue_sachiro <sachiro.inoue@drecom.co.jp>
2017-01-16 19:42:24 +0900

    Update deps. But fail features/ui.feature because NCurses was updated.

--- Makefile
+++ Makefile
@@ -1,17 +1,21 @@
+.PHONY: help build init install test uninstall
 default: build
 
-build:
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .' $(MAKEFILE_LIST) | sort | awk -F ':.*?## ' '{printf "%s\t\t\t%s\n",$$1,$$2}'
+
+build: ## Build a release binary
 	crystal deps
 	crystal build --release bin/git-cleanup-branch.cr -o bin/git-cleanup-branch
 
-init:
+init: ## Install pre-requirements
 	crystal deps
 	bundle
 
-install:
+install: ## cp the binary to PATH
 	cp bin/git-cleanup-branch /usr/local/bin/
 
-test:
+test: ## Test
 	find src spec -name '*.cr' | xargs crystal tool format
 	crystal spec
 	if hash shellcheck 2> /dev/null ; then shellcheck -s sh bin/create-sample-git-repository.sh ; fi
@@ -19,7 +23,7 @@ test:
 	rm -f greenletters.log
 	bundle exec cucumber
 
-uninstall:
+uninstall: ## rm the installed binary
 	rm -f /usr/local/bin/git-cleanup-branch
 
 # vim: set noet:
--- features/support/env.rb
+++ features/support/env.rb
@@ -14,8 +14,27 @@ end
 # Monkey patch for NCurses.
 module Greenletters
   # Monkey patch for NCurses.
+  class Process
+    # Monkey patch for NCurses.
+    def process_output(handle)
+      @logger.debug "output ready #{handle.inspect}"
+      data = handle.readpartial(1024)
+      # output_buffer << data
+      output_buffer << data.gsub(/\e\[.*?[A-Za-z]/, '')
+      @history << data
+      @logger.debug format_input_for_log(data)
+      @logger.debug "read #{data.size} bytes"
+      handle_triggers(:bytes)
+      handle_triggers(:output)
+      flush_triggers!(OutputTrigger) if ended?
+      flush_triggers!(BytesTrigger) if ended?
+    end
+  end
+
+  # Monkey patch for NCurses.
   module CucumberHelpers
     def greenletters_massage_pattern(text)
+      # Regexp.new(Regexp.escape(text.strip.tr_s(" \r\n\t", " ")).gsub('\ ', '\s+'))
       Regexp.new(Regexp.escape(text.strip.tr_s(" \r\n\t", ' ')).gsub('\ ', '[\b\s]+'))
     end
   end
